40eb28963da17b9e2e31b557adc2ad28b794cf40
A-Danylevych <54603203+A-Danylevych@users.noreply.github.com>
2020-03-27 16:30:57 +0200

    Upload

---
+++ OP2.3.2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace OP2._3._2
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            String pathx, pathy;
+            pathx = @"C:\Users\anton\OneDrive\Desktop\New Text Document (9).json";
+            pathy = @"C:\Users\anton\OneDrive\Desktop\New Text Document (10).json";
+            String savepath = @"C:\Users\anton\OneDrive\Desktop\Dictionary.json";
+            Present(pathx, pathy,savepath);
+            
+            Console.ReadKey();
+        }
+        static async void Present(String pathx, String pathy, String path)
+        {
+            List<String> kx = await KeyList(pathx);
+            List<Int32> vx = await ValueList(pathx);
+            List<String> ky = await KeyList(pathy);
+            List<Int32> vy = await ValueList(pathy);
+            Dictionary<String, Int32> result = new Dictionary<string, int>();
+            foreach (string i in kx)
+            {
+                if (ky.Contains(i))
+                {
+                    if (vx[kx.IndexOf(i)] == vy[ky.IndexOf(i)])
+                    {
+                        result.Add(i, vx[kx.IndexOf(i)]);
+                    }
+                }
+            }
+            Save(result, path);
+
+            foreach (KeyValuePair<String, Int32> i in result)
+            {
+                Console.WriteLine(i.Key + ":" + i.Value + " is present in both x and y");
+            }
+        }
+        static async void Save(Dictionary<String, Int32> d,String path)
+        {
+            using(FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync<Dictionary<string, int>>(fs, d);
+            }
+        }
+        static async Task<List<String>> KeyList(String path)
+        {
+            List<String> keys = new List<string>();
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                Dictionary<String, Int32> x = await JsonSerializer.DeserializeAsync<Dictionary<string, int>>(fs);
+                foreach (KeyValuePair<String, Int32> i in x)
+                {
+                    keys.Add(i.Key);
+                }
+                return keys;
+            }
+        }
+        static async Task<List<Int32>> ValueList(String path)
+        {
+            List<Int32> values = new List<Int32>();
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                Dictionary<String, Int32> x = await JsonSerializer.DeserializeAsync<Dictionary<string, int>>(fs);
+                foreach (KeyValuePair<String, Int32> i in x)
+                {
+                    values.Add(i.Value);
+                }
+                return values;
+            }
+        }
+    }
+}
---
+++ OP2.3/CarParking.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OP2._3
+{
+    public class CarParking
+    {
+        public Dictionary<Int32, Car> Cars { get; private set; }
+        public Dictionary<Int32, DateTime> Time { get; private set; }
+        public List<Int32> Places { get; private set; }
+        public Int32 Capasity { get; private set; }
+        public Car GetCar(Int32 i)
+        {
+            if (Places.Contains(i))
+            {
+                Places.Remove(i);
+                TimeSpan time = DateTime.Now.Subtract(Time[i]);
+                int t = Convert.ToInt32(time.TotalSeconds)/3600;
+                if (t == 0)
+                {
+                    Console.WriteLine("You have to pay 10$");
+                }
+                else
+                {
+                    Console.WriteLine($"You have to pay {t + 10}$");
+                }
+                Car car= Cars[i];
+                Time.Remove(i);
+                Cars.Remove(i);
+                return car;
+            }
+            else
+            {
+                throw new Exception("There is no car with this number");
+            }
+        }
+        public Int32 ParkCar(Car car)
+        {
+            Int32 i = FindPlace();
+            Cars[i] = car;
+            Time[i] = DateTime.Now;
+            return i;
+        }
+        public Int32 FindPlace()
+        {
+            int max = 0;
+            for(Int32 i = 0; i < Places.Count; i++)
+            {
+                if (!Places.Contains(i))
+                {
+                    Places.Add(i);
+                    return i;
+                }
+                if (max < i)
+                {
+                    max = i;
+                }
+            }
+            if (max < Capasity)
+            {
+                Places.Add(max);
+                return max;
+            }
+            else
+            {
+                throw new ArgumentException("Parking if out of free space");
+            }
+            
+        }
+        public CarParking(Int32 capasity)
+        {
+            Cars = new Dictionary<int, Car>();
+            Time = new Dictionary<int, DateTime>();
+            Places = new List<int>();
+            if (capasity > 0)
+            {
+                Capasity = capasity;
+            }
+            else
+            {
+                throw new ArgumentException("capasity cannot be less then 1");
+            }
+
+        }
+    }
+}
